e1ee5d668132dcb6f6f43a2be658572f2410c139
DanLawrie <71726127+DanLawrie@users.noreply.github.com>
2021-11-04 20:05:59 -0400

    Add files via upload

---
+++ newBankAccount/src/newBankAccount/AccountTest.java
@@ -0,0 +1,30 @@
+package newBankAccount;
+
+public class AccountTest {
+
+	public static void main(String[] args) {
+	BankAccount first = new BankAccount("Nina Masters", "0001", 1000);
+	CheckingAccount second = new CheckingAccount("John Smith", "0002", 500);
+	SavingAccount third = new SavingAccount("Amy Smith", "0003", 15000, 3.5);
+	
+	
+	System.out.println(first);
+	System.out.println(second);
+	System.out.println(third);
+	
+	
+	second.deposit(10000);
+	double averageBalance = ((500*15) +(second.getBalance()*15))/30;
+	second.MonthEnd(averageBalance);System.out.println(averageBalance);
+	
+	
+	third.withdraw(5000);
+	averageBalance = ((15000*20)+(10000*10))/30;
+	third.MonthEnd(averageBalance);
+	
+	System.out.println(first);
+	System.out.println(second);
+	System.out.println(third);
+	}
+
+}
